a947673f79e6024328a0cdf729f0b913545b8091
LeggoHerway <ixcs.me@outlook.com>
2026-05-10 02:59:32 -0500

    Replace logo image with mine2pie

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
-import logoSharedrop from "@/assets/logo-sharedrop.png";
+import logoSharedrop from "@/assets/mine2pie.png";
 
 const statusMessages = ["Scanning...", "Securing...", "Connecting..."];
 
